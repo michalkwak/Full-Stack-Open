d3b642eb21fa41434f13a212ff2747d7b6c5715e
Michał Kwak <michal.kwak2806@gmail.com>
2026-06-15 15:04:50 +0300

    Exercise 2.7

--- Phonebook/src/App.jsx
+++ Phonebook/src/App.jsx
@@ -8,6 +8,10 @@ const App = () => {
 
   const addPerson = (event) => {
     event.preventDefault()
+    if (persons.some(person => person.name === newName)) {
+      alert(newName + ` is already added to phonebook`)
+      return
+    }
     console.log('button clicked', event.target)
     const personObject = {
       name: newName,
